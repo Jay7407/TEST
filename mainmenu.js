017d22b0b2274ea9c6679bf98afb433d82efc922
Jay7407 <84159545+Jay7407@users.noreply.github.com>
2021-05-13 20:32:58 -0500

    Add files via upload

---
+++ mainmenu.js
@@ -0,0 +1,53 @@
+
+
+var menu1 = new MENU("top");
+
+menu1.entry(1, 20, "<b>Home</b>",       "/index.asp", "");
+menu1.entry(1, 20, "<b>Information</b>",       "", "");
+menu1.entry(2, 20, "<b>Find Some Assistance</b>",     "/gethelp.asp", "");
+menu1.entry(2, 15, "<b>Calendar</b>",     "/calendar/index.asp", "");
+menu1.entry(2, 20, "<b>Grand Officers</b>",     "", "");
+menu1.entry(3, 20, "<b>Grand Floor Officers</b>",     "/GrandOfficers/indexof-19-20.asp", "");
+menu1.entry(3, 20, "<b>Elected Boards</b>",     "/GrandOfficers/BOD_Exam-19-20.asp", "");
+menu1.entry(3, 20, "<b>Deputies</b>",     "/GrandOfficers/", "");
+menu1.entry(4, 20, "<b>District 1</b>",     "/GrandOfficers/Deputies1.asp", "");
+menu1.entry(4, 20, "<b>District 2</b>",     "/GrandOfficers/Deputies2.asp", "");
+menu1.entry(4, 20, "<b>District 3</b>",     "/GrandOfficers/Deputies3.asp", "");
+menu1.entry(4, 20, "<b>District 4</b>",     "/GrandOfficers/Deputies4.asp", "");
+menu1.entry(4, 20, "<b>District 5</b>",     "/GrandOfficers/Deputies5.asp", "");
+menu1.entry(2, 20, "<b>Past Grand Officers</b>",     "/pastgrands/pastgrands.asp", "");
+menu1.entry(2, 20, "<b>Special Committees Duties</b>",       "/forms/committees/committees.pdf", "");
+menu1.entry(2, 20, "<b>About OES</b>",       "/about_oes.asp", "");
+menu1.entry(2, 15, "<b>Membership</b>",  "/membership.asp", "");
+menu1.entry(2, 15, "<b>Welcome to our New Member</b>",  "/forms/Welcome_New_Member.doc", "");
+menu1.entry(2, 15, "<b>History</b>",     "/history.asp", "");
+menu1.entry(2, 15, "<b>FAQ</b>",     "/faq.asp", "");
+menu1.entry(2, 20, "<b>Links</b>",   "", "");    
+menu1.entry(3, 20, "<b>Grand Chapters</b>", "links.asp", "");
+menu1.entry(3, 20, "<b>Texas Chapters </b>",   "Texas Chapters.asp", "");	
+menu1.entry(2, 15, "<b>Locate a Chapter</b>",     "/chapter_search.asp", "");
+menu1.entry(2, 15, "<b>O.E.S. Happenings</b>",     "", "");
+menu1.entry(3, 15, "<b>gcpict</b>",     "/District-happenings-2019-20/gc-2019-20/gcpict.asp", "");
+menu1.entry(3, 15, "<b>Dist.1</b>",     "/District-happenings-2019-20/District-1/District-1-happenings.asp", "");
+menu1.entry(3, 15, "<b>Dist.2</b>",     "/District-happenings-2019-20/District-2/District-2-happenings.asp", "");
+menu1.entry(3, 15, "<b>Dist.3</b>",     "/District-happenings-2019-20/District-3/District-3-happenings.asp", "");
+menu1.entry(3, 15, "<b>Dist.4</b>",     "/District-happenings-2019-20/District-4/District-4-happenings.asp", "");
+menu1.entry(3, 15, "<b>Dist.5</b>",     "/District-happenings-2019-20/District-5/District-5-happenings.asp", "");
+menu1.entry(1, 20, "<b>Board Minutes</b>",  "/minutes.asp", "");
+menu1.entry(1, 20, "<b>Forms</b>",     "", "");
+menu1.entry(2, 20, "<b>General Grand Chapter </b>",     "/GGC_forms.asp", "");
+menu1.entry(2, 20, "<b>Grand Chapter Texas</b>",     "/GC_forms.asp", "");
+menu1.entry(2, 20, "<b>Grand Chapter Session</b>", "/GC_session.asp#gc", "");
+menu1.entry(2, 15, "<b>Benevolent Aid</b>",  "/benevolent.asp", "");
+menu1.entry(2, 15, "<b>Scholarships</b>",  "/Scholar_forms.asp#sc", "");
+menu1.entry(2, 15, "<b>Grand Chapter Trip</b>",  "/forms/Trip_Files/trips.asp", "");
+menu1.entry(2, 15, "<b>Spring Fling </b>",  "/forms/Trip_Files/Spring Fling.asp", "");
+menu1.entry(2, 15, "<b>Exchange Visits </b>",  "/eightstate.asp", "");
+menu1.entry(1, 20, "<b>Contact Us</b>",       "/contact_us.asp", "");
+menu1.entry(1, 20, "<b>Secretary Interface</b>", "https://oestx.groupable.app/");
+
+menu1.mainTop = 0;
+menu1.mainLeft = 0;
+menu1.mainBGColor = "";
+menu1.floatMenu = true;
+menu1.create();
